90d68895efad8da0c1db6cbbdb29d59ac48aa67c
EdisonChenZX <1648458952@qq.com>
2022-10-12 16:34:11 +0800

    did.redpack bug修复

--- contracts/did.redpack/src/redpack.cpp
+++ contracts/did.redpack/src/redpack.cpp
@@ -95,14 +95,17 @@ void redpack::claimredpack( const name& claimer, const name& code, const string&
 
     fee_t fee_info(redpack_itr->total_quantity.symbol);
     CHECKC( _db.get(fee_info), err::FEE_NOT_FOUND, "fee not found" );
-    int64_t iden = 0;
+    
     if((redpack_type)redpack_itr->type == redpack_type::DID_RANDOM || (redpack_type)redpack_itr->type == redpack_type::DID_MEAN){
         auto claimer_acnts=amax::account_t::idx_t( fee_info.did_contract, claimer.value );
-        int64_t amount;
+        bool is_auth;
         for( auto claimer_acnts_iter = claimer_acnts.begin(); claimer_acnts_iter!=claimer_acnts.end(); claimer_acnts_iter++ ){
-            amount = amount + claimer_acnts_iter->balance.amount;
+            if(claimer_acnts_iter->balance.amount > 0){
+                is_auth = true;
+                break;
+            } 
         }
-        CHECKC( amount > iden,err::DID_NOT_AUTH, "did is not authenticated" );
+        CHECKC( is_auth, err::DID_NOT_AUTH, "did is not authenticated" );
     }
 
     claim_t::idx_t claim_tbl(_self, _self.value);
