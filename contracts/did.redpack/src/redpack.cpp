1e44cef7bd1d335b607f0f3b8a3b2d0fd932f908
EdisonChenZX <1648458952@qq.com>
2022-12-09 15:35:50 +0800

    contract bug fix

--- contracts/did.redpack/src/redpack.cpp
+++ contracts/did.redpack/src/redpack.cpp
@@ -174,7 +174,15 @@ void redpack::cancel( const name& code )
         TRANSFER_OUT(fee_info.contract_name, redpack.sender, cancelamt, string("red pack cancel transfer"));
     }
     _db.del(redpack);
+    // claim_t::idx_t claims(_self, _self.value);
+    // auto claims_index = claims.get_index<"packid"_n>();
+    // auto claims_iter = claims_index.find(code.value);
+    // while(claims_iter != claims_index.end()){
+    //     claims_index.erase(claims_iter);
+    //     claims_iter = claims_index.find(code.value);
+    // }
 }
+
 
 void redpack::delclaims( const uint64_t& max_rows )
 {
